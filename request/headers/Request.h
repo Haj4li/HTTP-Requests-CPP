6ae71040bbb5d268b1008471c7cae5812cdee8a6
Haj4li <48994331+Haj4li@users.noreply.github.com>
2023-05-26 22:23:05 +0330

    Update Request.h
    Add new request methods

--- request/headers/Request.h
+++ request/headers/Request.h
@@ -1,17 +1,38 @@
+
 #pragma once
 #include <windows.h>
 #include <wininet.h>
-#include <stdio.h>
+#include <string>
+#include <iostream>
+#pragma comment (lib, "Wininet.lib")
 //  Coded by Haj4li
 // https://github.com/Haj4li/HTTP-Requests-CPP/
-#pragma comment (lib, "Wininet.lib")
+class Response
+{
+public:
+	DWORD status_code = 0;
+	std::string text;
+	DWORD error = 0;
+};
 
 class Request
 {
+private:
+	HINTERNET hSession;
+	HINTERNET hConnect;
+	HINTERNET hHttpReq;
 
-	public:
-	
-	void Connect(const char* method, const char* url, const char* page, char* &buffer,const char headers[] = NULL,DWORD headers_len = NULL, const char data[] = NULL, DWORD data_len = NULL);
-		
+	DWORD createHandles(const char* server, const char* method, const char* page = NULL);
+	void closeHandles();
+	Response send();
+
+public:
+
+	INTERNET_PORT port = INTERNET_DEFAULT_HTTP_PORT;
+	std::string user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/109.0";
+	std::string proxy = "";
+
+	Response Get(const char* server, const char* page = NULL, const char headers[] = NULL);
+	Response Post(const char* server, const char* page, const char headers[], const char* data);
+
 };
-
