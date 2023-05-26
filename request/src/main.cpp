fd7e7565b32525c352f3c4953fea508fa0105c74
Haj4li <48994331+Haj4li@users.noreply.github.com>
2023-05-26 22:19:22 +0330

    Update main.cpp
    Change request methods

--- request/src/main.cpp
+++ request/src/main.cpp
@@ -1,23 +1,14 @@
 #include "Request.h"
 //  Coded by Haj4li
 // https://github.com/Haj4li/HTTP-Requests-CPP/
-int main(int argc, char* argv[]) {
-
-	const char ip[] = "127.0.0.1";
-
-	Request req;
-	char* buf = NULL;
-	const char headers[] = "Content-Type: application/x-www-form-urlencoded";
-	const char post[] = "data=test";
-
-	req.Connect("GET", ip, "test.php", buf,headers, sizeof(headers)-1);
-	printf("\nGET : %s\n", buf);
-	free(buf);
-
-	req.Connect("POST", ip, "test.php", buf, headers, sizeof(headers)-1,post,sizeof(post)-1);
-	printf("\nPOST : %s\n", buf);
-    
-	free(buf);
-
-	return 0;
+int main()
+{
+    const char headers[] = "Content-Type: application/x-www-form-urlencoded;Kos: yes";
+    Request req;
+    req.user_agent = "test";
+   
+    Response resp = req.Get("google.com", "");
+    std::cout << resp.text << std::endl;
+    std::cout << "Last Error : " << resp.error << std::endl;
+    std::cout << "Statue Code : " << resp.status_code << std::endl;
 }
