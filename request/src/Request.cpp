66720e5bbc582dcf30374775c963775c92ed6d50
Haj4li <48994331+Haj4li@users.noreply.github.com>
2023-05-26 22:18:31 +0330

    Update Request.cpp
    Fix some issues
    Add Get & Post Methods
    Return Response as an object

--- request/src/Request.cpp
+++ request/src/Request.cpp
@@ -1,94 +1,115 @@
 #include "Request.h"
 //  Coded by Haj4li
 // https://github.com/Haj4li/HTTP-Requests-CPP/
-void Request::Connect(const char* method, const char* url, const char* page,char* &buffer, const char headers[], DWORD headers_len, const char data[], DWORD data_len)
+DWORD Request::createHandles(const char* server, const char* method, const char* page)
 {
-	const char agent[] = "Mozilla/5.0";
+	hSession = InternetOpenA(user_agent.c_str(), (proxy.length() == 0) ? INTERNET_OPEN_TYPE_DIRECT : INTERNET_OPEN_TYPE_PROXY, (proxy.length() == 0) ? NULL : proxy.c_str(), NULL, 0);
+	if (hSession == NULL)
+		return GetLastError();
 
-	HINTERNET hSession = InternetOpenA(
-		agent,
-		INTERNET_OPEN_TYPE_DIRECT,
-		NULL,
-		NULL,
-		0);
-	HINTERNET hConnect = InternetConnectA(
-		hSession,
-		url,
-		80,
-		NULL,
-		NULL,
-		INTERNET_SERVICE_HTTP,
-		0,
-		0);
-	HINTERNET hHttpFile = HttpOpenRequestA(
-		hConnect,
-		method,
-		page,
-		"HTTP/1.1",
-		NULL,
-		NULL,
-		NULL,
-		0);
-	
+	hConnect = InternetConnectA(hSession, server, port, NULL, NULL, INTERNET_SERVICE_HTTP, 0, 0);
+	if (hConnect == NULL)
+	{
+		closeHandles();
+		return GetLastError();
+	}
+
+	hHttpReq = HttpOpenRequestA(hConnect, method, page, "HTTP/1.1", NULL, NULL, INTERNET_FLAG_NO_CACHE_WRITE | INTERNET_FLAG_RELOAD, 0);
+	if (hHttpReq == NULL)
+	{
+		closeHandles();
+		return GetLastError();
+	}
+	return 0;
+}
+
+void Request::closeHandles()
+{
+	if (hConnect)
+		InternetCloseHandle(hConnect);
+	if (hSession)
+		InternetCloseHandle(hSession);
+	if (hHttpReq)
+		InternetCloseHandle(hHttpReq);
+}
+
+Response Request::send()
+{
+	Response resp;
+	DWORD length = sizeof(DWORD);
+
+	HttpQueryInfoA(
+		hHttpReq,
+		HTTP_QUERY_STATUS_CODE | HTTP_QUERY_FLAG_NUMBER,
+		&resp.status_code,
+		&length,
+		NULL
+	);
+	DWORD resSize;
+	InternetQueryDataAvailable(hHttpReq, &resSize, NULL, NULL);
+	if (resSize == NULL)
+	{
+		resp.error = GetLastError();
+		closeHandles();
+		return resp;
+	}
+	resp.text.clear();
+	resp.text = "";
+	DWORD dwBytesRead;
+	char buffer[1024];
+	do
+	{
+		if (InternetReadFile(hHttpReq, &buffer, resSize + 1, &dwBytesRead)) {
+			if (dwBytesRead != 0)
+			{
+				resp.text.append(buffer, dwBytesRead);
+			}
+		}
+	} while (dwBytesRead != 0);
+	closeHandles();
+	return resp;
+}
+
+Response Request::Get(const char* server, const char* page, const char headers[])
+{
+	Response resp;
+	DWORD hs = NULL;
+	resp.error = createHandles(server, "GET", page);
+	if (resp.error != 0)
+		return resp;
+		
 	if (headers != NULL)
 	{
-		HttpAddRequestHeadersA(hHttpFile, headers, headers_len, HTTP_ADDREQ_FLAG_ADD | HTTP_ADDREQ_FLAG_REPLACE);
+		hs = std::string(headers).length();
+		HttpAddRequestHeadersA(hHttpReq, headers, hs, HTTP_ADDREQ_FLAG_ADD | HTTP_ADDREQ_FLAG_REPLACE);
 	}
-
-	if (!HttpSendRequestA(hHttpFile, headers, headers_len, (char*)data, data_len)) {
-		printf("HttpSendRequest error : (%lu)\n", GetLastError());
-
-		InternetErrorDlg(
-			GetDesktopWindow(),
-			hHttpFile,
-			ERROR_INTERNET_CLIENT_AUTH_CERT_NEEDED,
-			FLAGS_ERROR_UI_FILTER_FOR_ERRORS |
-			FLAGS_ERROR_UI_FLAGS_GENERATE_DATA |
-			FLAGS_ERROR_UI_FLAGS_CHANGE_OPTIONS,
-			NULL);
-
-		// close handle 
-		InternetCloseHandle(hHttpFile);
-		InternetCloseHandle(hConnect);
-		InternetCloseHandle(hSession);
-		return;
-	}
-
-	DWORD buffer_size;
-	InternetQueryDataAvailable(hHttpFile, &buffer_size, NULL, NULL);
-	if (buffer_size == NULL)
+	if (!HttpSendRequestA(hHttpReq, headers, hs, NULL, 0))
 	{
-		// close handle 
-		InternetCloseHandle(hHttpFile);
-		InternetCloseHandle(hConnect);
-		InternetCloseHandle(hSession);
-		return;
+		resp.error = GetLastError();
+		closeHandles();
+		return resp;
+	}
+	return send();
+}
+
+Response Request::Post(const char* server, const char* page, const char headers[], const char* data)
+{
+	Response resp;
+	DWORD hs= NULL;
+	resp.error = createHandles(server, "POST", page);
+	if (resp.error != 0)
+		return resp;
+	if (headers != NULL)
+	{
+		hs = std::string(headers).length();
+		HttpAddRequestHeadersA(hHttpReq, headers, hs, HTTP_ADDREQ_FLAG_ADD | HTTP_ADDREQ_FLAG_REPLACE);
 	}
 
-	buffer = new char[buffer_size + 1];
-
-	while (true) {
-		DWORD dwBytesRead;
-		BOOL bRead;
-
-		bRead = InternetReadFile(
-			hHttpFile,
-			buffer,
-			buffer_size + 1,
-			&dwBytesRead);
-
-		if (dwBytesRead == 0) break;
-
-		if (!bRead) {
-			printf("InternetReadFile error : <%lu>\n", GetLastError());
-		}
-		else {
-			buffer[dwBytesRead] = 0;
-			//printf("Retrieved %lu data bytes: %s\n", dwBytesRead, buffer);
-		}
+	if (!HttpSendRequestA(hHttpReq, headers, hs, (char*)data, std::string(data).length()))
+	{
+		resp.error = GetLastError();
+		closeHandles();
+		return resp;
 	}
-
-	InternetCloseHandle(hHttpFile);
-	InternetCloseHandle(hConnect);
-	InternetCloseHandle(hSession);
+	return send();
 }
